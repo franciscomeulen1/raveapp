9735234b6a92680f88e676f0d3dd95763c402a91
Joseluisrigolino <72703830+Joseluisrigolino@users.noreply.github.com>
2022-09-12 19:51:48 -0300

    Agregado de footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content fixed inset-x-0 bottom-0 mt-8">
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content fixed inset-x-0 bottom-0">
       <div>
         <p>Copyright © 2022 - Todos los derechos reservados por Rave-App</p>
       </div>
