cdccf9a8a54e1b2855c22c287bd49c9d19e91bfa
Francisco Juarez <francisco.fmj@hotmail.com>
2022-10-23 21:47:04 -0300

    evento

--- src/vistas/Evento.js
+++ src/vistas/Evento.js
@@ -6,26 +6,42 @@ import { BsGeoAltFill } from "react-icons/bs";
 export default function Evento() {
     return (
         <div>
-            <div className="px-10 mb-11">
+            <div className="sm:px-10 mb-11">
                 <NavBar />
 
-                <h1 className='px-10 mb-8 mt-2 text-3xl font-bold underline underline-offset-8'>Nombre del evento</h1>
+                <h1 className='mx-10 sm:px-10 mb-8 mt-2 text-3xl font-bold underline underline-offset-8'>Nombre del evento</h1>
 
-                <div className='grid sm:grid-cols-2 gap-10 mb-6 px-10'>
-                    <div className='sm:columns-1'>
-                        <div>
-                            <p>DESCRIPCION DEL EVENTO, dolor sit amet consectetur adipisicing elit. Similique ullam cumque, necessitatibus delectus id rerum voluptates doloremque quidem debitis blanditiis. Itaque laudantium dolores laboriosam quas. Voluptatum adipisci culpa itaque ab.</p>
+                <div className='grid sm:grid-cols-2 gap-10 mb-6 px-5 sm:px-10'>
+
+                    <div className='columns-1 pr-5'>
+                        <div className='mb-6 flex justify-center'>
+                            <img src="https://www.dondeir.com/wp-content/uploads/2018/09/fiesta-1.jpg" width="450" height="auto" alt="imagen de evento" className='rounded-xl' />
                         </div>
-                        <div>
+                        <div className='mb-2'>
                             <p><span className='font-bold'>Horario del evento:</span> 23:50hs a 07:00hs</p>
                         </div>
-                        <div>
-                            <p className='font-semibold'><BsGeoAltFill className='inline'/> Direccion</p>
+                        <div className='mb-6 flex justify-between items-center'>
+                            <p className='font-semibold'><BsGeoAltFill className='inline' /> Av. Cnel. Niceto Vega 6599, CABA</p>
+                            <button className='btn bg-cyan-600 rounded-full ml-3'>Cómo llegar</button>
                         </div>
                         <TablaDeEntradas />
-
-
                     </div>
+
+                    <div className='columns-1 pr-5'>
+                        <div className='mb-6'>
+                            <p>DESCRIPCION DEL EVENTO, dolor sit amet consectetur adipisicing elit. Similique ullam cumque, necessitatibus delectus id rerum voluptates doloremque quidem debitis blanditiis. Itaque laudantium dolores laboriosam quas. <br/> Voluptatum adipisci culpa itaque ab. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed laudantium culpa excepturi, qui vitae officia dolorem inventore voluptatem deserunt beatae? <br/> Incidunt corrupti fugiat ab vel eum voluptas odio quas voluptates</p>
+                        </div>
+
+                        <iframe title="musicaSoundCloud" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1406208106&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" className='mb-6'>
+                        </iframe>
+
+                        
+                    </div>
+
+                    <div className='columns-1 pr-5'>
+                    <iframe height="315" src="https://www.youtube.com/embed/zmqS5hEi_QI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className='w-full '></iframe>
+                    </div>
+
                 </div>
 
             </div>
