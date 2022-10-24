7be9e8139bcccfc12a2d834e43540e4e015f67f0
Francisco Juarez <francisco.fmj@hotmail.com>
2022-10-23 21:52:26 -0300

    card linkeada a evento

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import '../index.css';
+import { Link } from 'react-router-dom';
 export default function Card() {
     return (
         <div className="p-5 inline-flex">
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <Link className="card w-96 bg-base-100 shadow-xl" to='/evento'>
                 <figure><img src="https://t3.ftcdn.net/jpg/02/87/35/70/240_F_287357045_Ib0oYOxhotdjOEHi0vkggpZTQCsz0r19.jpg" alt="Joda" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">
@@ -15,7 +16,7 @@ export default function Card() {
                         <div className="badge badge-outline">LGBT</div>
                     </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
